6f57b118d9fcfef568d525d590eb0653c86a49ed
GrainyTV <n.arpad@protonmail.com>
2023-07-12 09:45:54 +0200

    Template F# File

---
+++ Program.fs
@@ -0,0 +1,40 @@
+open System
+open System.IO
+
+//
+// Print Task Number
+//
+let TaskNumber (id: int, newLine: bool) = 
+    if newLine = true then
+        printfn ""
+        printfn "%d. feladat:" id
+    else
+        printfn "%d. feladat:" id
+
+//
+// Solution of Task 1
+//
+
+//
+// Solution of Task 2
+//
+
+//
+// Solution of Task 3
+//
+
+//
+// Solution of Task 4
+//
+
+//
+// Solution of Task 5
+//
+
+//
+// Solution of Task 6
+//
+
+//
+// Solution of Task 7
+//
